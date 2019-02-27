0e4600ceba063b733c238be550fd779472e3afd8
Roopali0 <42017295+Roopali0@users.noreply.github.com>
2019-02-27 23:16:40 +1100

    Add files via upload

---
+++ Reverse_String/Reverse_String/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_String
+{
+    
+class Program
+{
+    static void Main(string[] args)
+    {
+        string reversed_String = " ";
+        Console.WriteLine("Please write a string");
+        string name = Console.ReadLine();
+
+        for (int i = (name.Length - 1); i >= 0; i--)
+        {
+            reversed_String += name[i].ToString();
+        }
+        Console.Write("The reversed string is" + reversed_String);
+
+        Console.ReadKey();
+
+        
+
+     } // End of Main
+} // End of class
+    } // End of namespace
