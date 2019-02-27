7f917fd76eb7b4f741ccfcef26c3126fdfba98e4
Roopali0 <42017295+Roopali0@users.noreply.github.com>
2019-02-27 23:18:06 +1100

    Add files via upload

---
+++ Smallest_Number/Smallest_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smallest_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new int[8] { 2, 15, 3, 5, 6, 6, -1, 10 };
+            int i, j, Smallest = 0;
+
+            for (i = 0; i <= 7; i++)
+            {
+                for (j = 1; j <= 6; j++)
+                {
+                    if (num[j] <= num[j + 1])
+                    {
+                        int temp = num[j];
+                        num[j] = num[j + 1];
+                        num[j + 1] = temp;
+                    } // End of If
+                } // End of For 
+                Smallest = num[j];
+
+            } // End of For
+
+            Console.WriteLine("Smallest number is " + Smallest);
+            Console.Read();
+
+        } // End of Main
+    } // End of class
+} // End of namespace
+
